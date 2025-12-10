95d14e7626f24ed09773a8b22ece4980df758365
Bekzod1311 <bekzod.khusanov.coder@mail.ru>
2025-12-10 15:33:04 +0500

    Add files via upload

---
+++ Section-1 Computer and Program/input().py
@@ -0,0 +1,24 @@
+""" ism = input("ismingiz nima? ")
+print("Assalom alaykum " + ism)
+ism = input("Ismingiz nima?\n")
+print(f"Assalomu alaykum, {ism.title()}") """
+
+""" kocha="Bog'bon"
+mahalla="Sag'bon"
+tuman="Bodomzor"
+viloyat="Samarqand"
+print(f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati") """
+
+""" kocha = input("Ko'changizni nomi? ")
+mahalla = input("Mahallangizni nomi? ")
+tuman = input("Tumaningizni nomi? ")
+viloyat = input("Viloyatingizni nomi? ")
+print()
+print(f"{kocha} ko'chasi, \n{mahalla} mahallasi, \n{tuman} tumani, \n{viloyat} viloyati")
+print("="*40)
+manzil = f"{kocha} ko'chasi, \n{mahalla} mahallasi, \n{tuman} tumani, \n{viloyat} viloyati"
+print(manzil.title())
+print()
+print(manzil.capitalize())
+print()
+print(manzil.lower())"""
